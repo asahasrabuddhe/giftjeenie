bfc8714a38c7624f089b0e9a7b26702c9680ef6f
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2016-05-14 02:05:14 +0530

    sql update

--- config.php
+++ config.php
@@ -4,8 +4,8 @@
  * These are the database login details
  */
 define("HOST", "ap-cdbr-azure-east-c.cloudapp.net");     	// The host you want to connect to.
-define("USER", "b85414a43b2f48");    			// The database username. 
-define("PASSWORD", "866b237a");	// The database password. 
+define("USER", "b0a886fc0eead8");    			// The database username. 
+define("PASSWORD", "68ca1516");	// The database password. 
 define("DATABASE", "giftjeeni");	// The database name.
  
 define("DEFAULT_ROLE", "member");
